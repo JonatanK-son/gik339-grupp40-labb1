183cc5ab73ec850da7946cb38a760921533e113b
JonatanK-son <kontra.kson@gmail.com>
2024-12-05 13:03:57 +0100

    added functions and eventlisteners

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const checkbox = document.querySelector(".checkbox");
-const textfields = document.getElementsByClassName("textfield");
-const button = document.querySelector("button");
+const textfields = document.querySelectorAll(".textfield");
+const button = document.getElementById("remove");
+const outputDiv = document.getElementById("div2")
+
+function eavesdroppingBastard1(e) {
+    console.log(e.target)
+    let name = e.target.getAttribute("name")
+    console.log(name)
+}
+
+textfields.forEach(function(i) {
+    i.addEventListener("input", eavesdroppingBastard1)
+    i.addEventListener("blur", eavesdroppingBastard1)
+});
+
+checkbox.addEventListener("input", eavesdroppingBastard1)
 
 console.log(checkbox)
 console.log(textfields)
 console.log(button)
+console.log(outputDiv)
