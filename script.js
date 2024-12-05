c1bf611bbbc085dfefc3f0180f851cf8d5aabacd
JonatanK-son <kontra.kson@gmail.com>
2024-12-05 12:17:48 +0100

    moved script tag and added some variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const checkbox = document.querySelector(".checkbox");
+const textfields = document.getElementsByClassName("textfield");
+const button = document.querySelector("button");
+
+console.log(checkbox)
+console.log(textfields)
+console.log(button)
